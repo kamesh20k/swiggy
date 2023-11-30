32565d4989942c544731e308999e9c6ba84e9ab6
“kamesh20” <kamesh20k@gmail.com>
2023-11-30 14:32:21 +0000

    2ndcommit

--- swiggy.java
+++ swiggy.java
@@ -2,7 +2,7 @@ public class QuotientRemainder {
 
   public static void main(String[] args) {
 
-    int dividend = 25, divisor = 4;
+    int dividend = 25, divisor = 5;
 
     int quotient = dividend / divisor;
     int remainder = dividend % divisor;
